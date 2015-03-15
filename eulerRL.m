9cdbadde15ed41a3e92262d98d28f43dc00891de
mattinmir <mattinmir@gmail.com>
2015-03-15 19:28:58 +0000

    works

--- eulerRL.m
+++ eulerRL.m
@@ -1,1 +1,16 @@
-function Xnew = eulerRL(Xcurr, h, )
+function y_new = eulerRL(func, t0, y0, tn, h)
+   t = t0 : h :tn;
+   y(1) = y0;
+   
+   for i = 1:length(t)-1
+        y(i+1) = y(i) + h * feval(func, t(i), y(i));
+
+   end
+    plot (t,y);
+    xlabel('time')
+    ylabel('y')
+    disp(y(end))
+    y_new = y;
+    
+end
+
